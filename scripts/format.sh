88f7c9f486751d17fe4d7b5250e04d51cbec71ef
Christian <christian@lefty.io>
2019-11-01 09:37:50 +0100

    Changes

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,7 +5,19 @@ set -e
 declare -a sources=(
     "structs/base/base_plugin.h"
     "structs/base/base_plugin.cc"
+    "structs/base/code_builder.h"
+    "structs/base/code_builder.cc"
+    "structs/base/utils.h"
+    "structs/base/utils.cc"
 
+    "structs/bigquery/bigquery_plugin.h"
+    "structs/bigquery/bigquery_plugin.cc"
+    "structs/bigquery/protoc_gen_structs_bigquery_schema.cc"
+
+    "structs/cassandra/renderers/cql_file.h"
+    "structs/cassandra/renderers/cql_file.cc"
+    "structs/cassandra/renderers/java_file.h"    
+    "structs/cassandra/renderers/java_file.cc"    
 )
 
 for val in "${sources[@]}"; do
